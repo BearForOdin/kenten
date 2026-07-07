c80de4c3b8b834f90385dd7788c1dedf5a36b17a
BearForOdin <selasidzeamesi82@gmail.com>
2026-07-06 21:19:26 -0700

    cards

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import Features from "../components/Home/Features"
 import Hero from "../components/Home/Hero"
+import HomeCategories from "../components/Home/HomeCategories"
+import PopularProducts from "../components/Home/PopularProducts"
 
 
 const Home = () => {
@@ -7,6 +9,8 @@ const Home = () => {
     <div className="min-h-screen max-w-7wl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <Hero />
       <Features />
+      <HomeCategories />
+      <PopularProducts />
     </div>
   )
 }
